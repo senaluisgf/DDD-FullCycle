202e13b94bc0396dfbf8331545d255b56e0ced70
Luis "Shadow" Sena <sena.luisgf@gmail.com>
2024-09-22 12:39:16 -0400

    feat: :sparkles: starting customer implementation

---
+++ src/customer.ts
@@ -0,0 +1,27 @@
+class Customer {
+  private _id: string;
+  private _name: string;
+  private _address: string;
+  private _active: boolean = false;
+
+  constructor(id: string, name: string, address: string) {
+    this._id = id;
+    this._name = name;
+    this._address = address;
+  }
+
+  changeName(name: string) {
+    this._name = name;
+  }
+
+  activate() {
+    this._active = true;
+  }
+  deactivate() {
+    this._active = false;
+  }
+
+  get name(): string {
+    return this._name
+  }
+}
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-export function main(name: string): string {
-  console.log(`Hello, ${name}`);
-  return `Hello, ${name}`;
-}
