d8112a63821e6499d41cdb2e01cdc31aa4924cf5
Luis "Shadow" Sena <sena.luisgf@gmail.com>
2024-09-23 07:47:09 -0400

    refactor: :safety_vest: add customer id, name and activate validations

--- src/customer.ts
+++ src/customer.ts
@@ -1,20 +1,33 @@
 class Customer {
   private _id: string;
   private _name: string;
-  private _address: string;
+  private _address: string = "";
   private _active: boolean = false;
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string, name: string) {
     this._id = id;
     this._name = name;
-    this._address = address;
+    this.validate();
+  }
+
+  validate() {
+    if (!this._name.length) {
+      throw new Error("Name is required");
+    }
+    if (!this._id.length) {
+      throw new Error("Id is required");
+    }
   }
 
   changeName(name: string) {
     this._name = name;
+    this.validate();
   }
 
   activate() {
+    if (!this._address.length) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
     this._active = true;
   }
   deactivate() {
