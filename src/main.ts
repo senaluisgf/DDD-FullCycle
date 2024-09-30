a404183614d3bcdb741ab40283b56b9315aebc72
Luis "Shadow" Sena <sena.luisgf@gmail.com>
2024-09-30 08:01:48 -0400

    refactor: :lipstick:

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import Address from "./entity/address";
-import Customer from "./entity/customer";
-import Order from "./entity/order";
-import OrderItem from "./entity/order_item";
+import Address from "./domain/entity/address";
+import Customer from "./domain/entity/customer";
+import Order from "./domain/entity/order";
+import OrderItem from "./domain/entity/order_item";
 
 const customer1 = new Customer('c1', 'Luis Sena');
 const address1 = new Address('rua 1', 1, "Manaus", "Brasil", '12345-678');
