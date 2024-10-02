8efeb9f3ac18ffee53572a46ed579a529033418f
Luis "Shadow" Sena <sena.luisgf@gmail.com>
2024-10-02 19:26:21 -0400

    feat: :white_check_mark: finish customer repository

--- src/infrastructure/repository/customer.repository.ts
+++ src/infrastructure/repository/customer.repository.ts
@@ -1,3 +1,4 @@
+import Address from "../../domain/entity/address";
 import Customer from "../../domain/entity/customer";
 import CustomerRepositoryInterface from "../../domain/repository/customer.repository.interface";
 import CustomerModel from "../db/sequelize/model/customer.model";
@@ -18,14 +19,67 @@ export default class CustomerRepository implements CustomerRepositoryInterface {
   }
 
   async update(entity: Customer): Promise<void> {
-    return
+    try {
+      await CustomerModel.findByPk(entity.id);
+    } catch (error) {
+      throw new Error("Customer not found");
+    }
+
+    await CustomerModel.update({
+      name: entity.name,
+      active: entity.isActive(),
+      rewardPoints: entity.rewardPoints,
+      street: entity.address.street,
+      number: entity.address.number,
+      city: entity.address.city,
+      country: entity.address.country,
+      zipcode: entity.address.zipcode,
+    }, { where: { id: entity.id } });
   }
 
   async find(id: string): Promise<Customer> {
-    return
+    let customerModel;
+    try {
+      customerModel = await CustomerModel.findByPk(id);
+    } catch (error) {
+      throw new Error('Customer not found');
+    }
+
+    const customer = new Customer(customerModel.id, customerModel.name);
+    customer.addRewardPoints(customerModel.rewardPoints);
+    customer.addRewardPoints(customerModel.rewardPoints);
+    const address = new Address(
+      customerModel.street,
+      customerModel.number,
+      customerModel.city,
+      customerModel.country,
+      customerModel.zipcode,
+    )
+    customer.changeAddress(address);
+    if (customerModel.active) {
+      customer.activate();
+    }
+
+    return customer;
   }
 
   async findAll(): Promise<Customer[]> {
-    return
+    const customerModels = await CustomerModel.findAll();
+    return customerModels.map((customerModel) => {
+      const customer = new Customer(customerModel.id, customerModel.name);
+      customer.addRewardPoints(customerModel.rewardPoints);
+      const address = new Address(
+        customerModel.street,
+        customerModel.number,
+        customerModel.city,
+        customerModel.country,
+        customerModel.zipcode,
+      );
+      customer.changeAddress(address)
+      if (customerModel.active) {
+        customer.activate()
+      }
+      return customer;
+    })
   }
 }
